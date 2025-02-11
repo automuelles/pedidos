a8d5b0b782a5e3056c4c12ff9e4e2eea4fe4a865
Automuelles2@gmail.com <Automuelles2@gmail.com>
2025-02-11 10:09:56 -0500

    se modifica la pagina de novedades

--- Bodega/ReporteFinal.php
+++ Bodega/ReporteFinal.php
@@ -97,16 +97,7 @@ if ($transaccion > 0 && $documento > 0) {
 
 <body class="bg-gray-200 min-h-screen flex flex-col items-center justify-center">
     <!-- Header -->
-    <div class="neumorphism w-full max-w-xs p-6 text-center mb-6">
-        <h1 class="text-yellow-600 text-2xl font-bold">Bienvenido a Automuelles</h1>
-        <?php if (isset($_SESSION['user_name'])): ?>
-            <h1 class="text-black-600 text-2xl font-bold"><?php echo htmlspecialchars($_SESSION['user_name']); ?>!</h1>
-        <?php else: ?>
-            <h1 class="text-black-600 text-2xl font-bold">No estás autenticado.</h1>
-        <?php endif; ?>
-        <h1 class="text-black-600 text-2xl font-bold">Bodega</h1>
-    </div>
-
+    
     <div class="w-full max-w-xs pb-16">
         <div class="w-full max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md mt-10 pb-24"> <!-- Agregar pb-24 para dar espacio abajo -->
             <h1 class="text-2xl font-bold text-gray-800 mb-4">Detalles de la Factura</h1>
@@ -166,29 +157,7 @@ if ($transaccion > 0 && $documento > 0) {
             </form>
         </div>
     </div>
-    <!-- Footer Navigation -->
-    <nav class="fixed bottom-0 left-0 right-0 bg-white shadow-lg">
-        <div class="flex justify-around py-2">
-            <a href="../php/logout_index.php" class="text-blue-500 text-center flex flex-col items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12h18M9 5l7 7-7 7" />
-                </svg>
-                <span class="text-xs">Salir</span>
-            </a>
-            <a href="Bodega.php" class="text-gray-500 text-center flex flex-col items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                </svg>
-                <span class="text-xs">Volver</span>
-            </a>
-            <a href="#" id="openModal" class="text-gray-500 text-center flex flex-col items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                </svg>
-                <span class="text-xs">Apps</span>
-            </a>
-        </div>
-    </nav>
+    
 </body>
 
 </html>
